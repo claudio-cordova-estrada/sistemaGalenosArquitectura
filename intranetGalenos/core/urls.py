13e827adb54da478c271dc539f072f9004bc9031
claudio-cordova-estrada <claudionicolascordova@gmail.com>
2024-01-28 23:25:47 -0300

    about creado, base cambiada
    faltaba pagina about
    
    falta arreglar navbar

--- intranetGalenos/core/urls.py
+++ intranetGalenos/core/urls.py
@@ -4,13 +4,14 @@ from django.contrib import admin
 
 urlpatterns = [
     path('', home, name="home"),
+    path('about', about, name="about"),
     path('appointment', appointment, name="appointment"),
     path('contact', contact, name="contact"),
-    path('', logReg, name="logReg"),
-    path('', price, name="price"),
-    path('', service, name="service"),
-    path('', team, name="team"),
-    path('', testimonial, name="testimonial"),
+    path('logReg', logReg, name="logReg"),
+    path('price', price, name="price"),
+    path('service', service, name="service"),
+    path('team', team, name="team"),
+    path('testimonial', testimonial, name="testimonial"),
 ]
 
 admin.site.site_header = "Administrador"
--- intranetGalenos/core/views.py
+++ intranetGalenos/core/views.py
@@ -5,6 +5,9 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'core/home.html')
 
+def about(request):
+    return render(request, 'core/about.html')
+
 def appointment(request):
     return render(request, 'core/appointment.html')
 
