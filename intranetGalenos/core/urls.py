4317ecbbbb8eb8247b360cb4b667968561b0b2ad
claudio-cordova-estrada <claudionicolascordova@gmail.com>
2024-01-28 23:06:12 -0300

    conexion y models
    se crean las conexiones para poder levantar el servidor, además se traslada los modelos creados en la carpeta antigua a la nueva carpeta

--- intranetGalenos/core/urls.py
+++ intranetGalenos/core/urls.py
@@ -4,6 +4,13 @@ from django.contrib import admin
 
 urlpatterns = [
     path('', home, name="home"),
+    path('appointment', appointment, name="appointment"),
+    path('contact', contact, name="contact"),
+    path('', logReg, name="logReg"),
+    path('', price, name="price"),
+    path('', service, name="service"),
+    path('', team, name="team"),
+    path('', testimonial, name="testimonial"),
 ]
 
 admin.site.site_header = "Administrador"
--- intranetGalenos/core/views.py
+++ intranetGalenos/core/views.py
@@ -5,3 +5,23 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'core/home.html')
 
+def appointment(request):
+    return render(request, 'core/appointment.html')
+
+def contact(request):
+    return render(request, 'core/contact.html')
+
+def price(request):
+    return render(request, 'core/price.html')
+
+def service(request):
+    return render(request, 'core/service.html')
+
+def team(request):
+    return render(request, 'core/team.html')
+
+def testimonial(request):
+    return render(request, 'core/testimonial.html')
+
+def logReg(request):
+    return render(request, 'registration/logReg.html')
